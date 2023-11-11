7928bb1c14edd480c5a59bcb8444ee7246855cc2
Aaron <daaroncg135@gmail.com>
2023-11-11 17:23:35 -0600

    Video 4 Creacion del Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
