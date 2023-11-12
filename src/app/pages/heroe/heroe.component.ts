2ec8b7ec4fc43dccba39511774024af1248d24e2
Aaron <daaroncg135@gmail.com>
2023-11-11 18:32:45 -0600

    Video 7

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/hereo.model';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent {
+export class HeroeComponent implements OnInit{
+  ngOnInit(): void {
 
+  }
+
+  heroe:  HeroeModel= new HeroeModel();
+
+  constructor()   {}
+
+  guardar( form: NgForm ) {
+
+    if( form.invalid){
+      console.log('Formulario no válido');
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe);
+
+  }
 }
